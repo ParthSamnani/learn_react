dd006d1248450cde26ad39455545c60384f80526
Parth Samnani <samnaniparth@gmail.com>
2020-07-30 20:00:22 +0530

    learning tasks

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ import Counter from './components/Counter'
 function App() {
   return (
     <div className="App">
-      <Counter/>
-      {/* <Greet name = "Bruce" heroName = "Batman">
+      {/* <Counter/> */}
+      <Greet name = "Bruce" heroName = "Batman">
         <p>This is children props</p>
       </Greet>
       <Greet name = "Clark" heroName = "Superman">
         <button>Action</button>
         </Greet>
       <Greet name = "Diana" heroName = "Wonder woman" />
-      <Welcome name = "Bruce" heroName = "Batman"/>
+      {/* <Welcome name = "Bruce" heroName = "Batman"/>
       <Welcome name = "Clark" heroName = "Superman"/>
       <Welcome name = "Diana" heroName = "Wonder woman"/> */}
       {/* <Message></Message> */}
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const Greet = (props) => {
-    console.log(props)
-    return (
+const Greet = ({name, heroName}) => {
+  return (
         <div>
-        <h1>Hello {props.name} a.k.a {props.heroName}</h1>
-        {props.children}
+        <h1>Hello {name} a.k.a {heroName}</h1>
         </div>
         )
     
