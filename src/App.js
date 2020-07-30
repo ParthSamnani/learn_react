073cc07953013328257be4f4efe550bf21d61c9d
Parth Samnani <samnaniparth@gmail.com>
2020-07-30 19:20:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet'
+import Welcome from './components/Welcome'
+import Message from './components/Message'
+import Counter from './components/Counter'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter/>
+      {/* <Greet name = "Bruce" heroName = "Batman">
+        <p>This is children props</p>
+      </Greet>
+      <Greet name = "Clark" heroName = "Superman">
+        <button>Action</button>
+        </Greet>
+      <Greet name = "Diana" heroName = "Wonder woman" />
+      <Welcome name = "Bruce" heroName = "Batman"/>
+      <Welcome name = "Clark" heroName = "Superman"/>
+      <Welcome name = "Diana" heroName = "Wonder woman"/> */}
+      {/* <Message></Message> */}
     </div>
   );
-}
+}     
 
 export default App;
