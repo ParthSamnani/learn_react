0082123fb6af5687d2559abb159ad79b830fe039
Parth Samnani <samnaniparth@gmail.com>
2020-08-01 01:11:24 +0530

    event handling

--- src/App.js
+++ src/App.js
@@ -4,21 +4,27 @@ import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Message from './components/Message'
 import Counter from './components/Counter'
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
 
 function App() {
   return (
     <div className="App">
+      <EventBind></EventBind>
+      {/* <FunctionClick></FunctionClick> */}
+      {/* <ClassClick></ClassClick> */}
       {/* <Counter/> */}
-      <Greet name = "Bruce" heroName = "Batman">
+      {/* <Greet name = "Bruce" heroName = "Batman">
         <p>This is children props</p>
-      </Greet>
-      <Greet name = "Clark" heroName = "Superman">
+      </Greet> */}
+      {/* <Greet name = "Clark" heroName = "Superman">
         <button>Action</button>
-        </Greet>
-      <Greet name = "Diana" heroName = "Wonder woman" />
-      <Welcome name = "Bruce" heroName = "Batman"/>
-      <Welcome name = "Clark" heroName = "Superman"/>
-      <Welcome name = "Diana" heroName = "Wonder woman"/>
+        </Greet> */}
+      {/* <Greet name = "Diana" heroName = "Wonder woman" /> */}
+      {/* <Welcome name = "Bruce" heroName = "Batman"/> */}
+      {/* <Welcome name = "Clark" heroName = "Superman"/> */}
+      {/* <Welcome name = "Diana" heroName = "Wonder woman"/> */}
       {/* <Message></Message> */}
     </div>
   );
