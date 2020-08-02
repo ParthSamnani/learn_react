3f4f09cc38f6b504d39ea5bd963a57e554993d6d
Parth Samnani <samnaniparth@gmail.com>
2020-08-02 18:11:19 +0530

    conditional rendering of UI

--- src/App.js
+++ src/App.js
@@ -7,11 +7,15 @@ import Counter from './components/Counter'
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
 import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
+import UserGreeting from './components/UserGreeting';
 
 function App() {
   return (
     <div className="App">
-      <EventBind></EventBind>
+      <UserGreeting/>
+      {/* <ParentComponent/> */}
+      {/* <EventBind></EventBind> */}
       {/* <FunctionClick></FunctionClick> */}
       {/* <ClassClick></ClassClick> */}
       {/* <Counter/> */}
