7d47142380269cb45a0f04848f4082a0ef7cfe2c
Parth Samnani <samnaniparth@gmail.com>
2020-08-06 19:45:04 +0530

    Updates

---
+++ precache-manifest.4e22a299f15a76a4ecef90c8b74d93eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1e708b2943cd67a230e388e2b8a61c2",
+    "url": "/ParthSamnani/learn_react/index.html"
+  },
+  {
+    "revision": "5930c3e21115c8eb9e2a",
+    "url": "/ParthSamnani/learn_react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "68e46719533387f5f5b2",
+    "url": "/ParthSamnani/learn_react/static/js/2.68c8ac0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ParthSamnani/learn_react/static/js/2.68c8ac0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5930c3e21115c8eb9e2a",
+    "url": "/ParthSamnani/learn_react/static/js/main.f153c9e5.chunk.js"
+  },
+  {
+    "revision": "a74c03df6ce2a6592e1a",
+    "url": "/ParthSamnani/learn_react/static/js/runtime-main.6105eaf9.js"
+  }
+]);
